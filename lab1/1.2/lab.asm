d6ca319a418fba9392ca140a88257a4b1485be72
7D24H <dada7d24h@163.com>
2018-04-16 01:22:30 +0800

    output 0 1 success

--- lab1/1.2/lab.asm
+++ lab1/1.2/lab.asm
@@ -4,6 +4,10 @@ section .data
 	endl dd 0xa	;换行
 	input dd 0	;就是一个用来存储每次输入的（十进制）数字的容器
 	pushNum dd 0	;压栈次数
+	loopInitial:dd 99	;初始化fibA fibB用的循环计数器
+	zero db '0',0xa
+	one db '1',0xa	
+	fibN:dd 0	;计算到第几个fib值	
 	
 section .bss
 	buff resb 1	;每次输入的字符地址
@@ -12,7 +16,10 @@ section .bss
 	
 	fibA resd 100	;相当于fib中的f(n-2)
 	fibB resd 100 	;相当于fib中的f(n-1)
-
+	
+	aPointer resd 1	;fibA的地址
+	bPointer resd 1	;fibB的地址
+	
 	output resd 1	;输出地址的单元
 
 section .text
@@ -54,42 +61,43 @@ saveX:	mov eax,[input]
 	mov [varX],eax
 	mov eax,0
 	mov [input],eax	;input清零好准备存下一个十进制数
-	jmp pushX
+;	jmp pushX	;这里是测试stack的 已经成功
+	jmp read	;继续读取Y
 
     ;----------------------------------------
 	;用除十取余的方法输出
-pushX:	mov eax,[varX] ;被除数
-        mov ebx,10      ;除数
-        mov rdx,0       ;初始化余数
-        div ebx
-        add edx,0x30    ;余数以ASCII码输出
-	mov [varX],eax	;商存回去
-    	
-	push rdx	;把余数压栈
-	mov ecx,[pushNum]
-	inc ecx		;压栈次数要自增
-	mov [pushNum],ecx
-	
-	cmp eax,0	;被除数变为0说明已经除完了
-	je writeX	;就要出栈输出了
-	jmp pushX	;否则继续压栈
+;pushX:	mov eax,[varX] ;被除数
+ ;       mov ebx,10      ;除数
+  ;      mov rdx,0       ;初始化余数
+   ;     div ebx
+    ;    add edx,0x30    ;余数以ASCII码输出
+;	mov [varX],eax	;商存回去
+ ;   	
+;	push rdx	;把余数压栈
+;	mov ecx,[pushNum]
+;	inc ecx		;压栈次数要自增
+;	mov [pushNum],ecx
+;	
+;	cmp eax,0	;被除数变为0说明已经除完了
+;	je writeX	;就要出栈输出了
+;	jmp pushX	;否则继续压栈
 
-writeX:	pop rcx
-	mov [output],ecx
-	mov ecx,[pushNum]
-	dec ecx
-	mov [pushNum],ecx
+;writeX:	pop rcx
+;	mov [output],ecx
+;	mov ecx,[pushNum]
+;	dec ecx
+;	mov [pushNum],ecx
 
-	mov eax,4
-	mov ebx,1
-	mov ecx,output
-	mov edx,4
-	int 80h
+;	mov eax,4
+;	mov ebx,1
+;	mov ecx,output
+;	mov edx,4
+;	int 80h
 
-	mov ecx,[pushNum]
-	cmp ecx,0
-	je read
-	jmp writeX
+;	mov ecx,[pushNum]
+;	cmp ecx,0
+;	je read
+;	jmp writeX
     ;-----------------------------------------
 	;这里只是为了测试x有没有存进varX，所以输出varX中数对应的ASCII（可能会溢出），
 	;其实这里并不用输出的
@@ -114,22 +122,129 @@ saveY:  mov eax,[input]
         mov [input],eax
        ;-------------------
 	;待删
-        mov eax,4       ;输出从键盘输入的东西
-        mov ebx,1
-        mov ecx,varY
-        mov edx,1
-        int 80h
-
-	mov eax,4       ;换行
-        mov ebx,1
-        mov ecx,endl
-        mov edx,1
-        int 80h
+     ;   mov eax,4       ;输出从键盘输入的东西
+    ;    mov ebx,1
+   ;     mov ecx,varY
+  ;      mov edx,1
+ ;       int 80h
+;
+;	mov eax,4       ;换行
+    ;    mov ebx,1
+   ;     mov ecx,endl
+  ;      mov edx,1
+ ;       int 80h
 
        ;------------------
-	 jmp exit 	;要改的 这里应该进入正式的fib计算
+	 jmp startFib 	;要改的 这里应该进入正式的fib计算
 
 ;-----------------------------------------------------------
+
+startFib:
+	mov eax,fibA
+	mov [aPointer],eax
+	mov ebx,fibB
+	mov [bPointer],ebx
+
+	mov eax,0
+	mov [fibA],eax	;初始化fib(0)=0
+	mov ebx,0
+	mov [fibB],ebx	;初始化fib(1)=1
+
+initialAB:
+	mov eax,[loopInitial]
+	cmp eax,0
+	je endIni
+	
+	mov eax,[aPointer]	;继续初始化fibA
+	add eax,4		;往前走 继续初始化为0
+	mov [aPointer],eax
+	mov ecx,[aPointer]	;ecx里放的是要初始为0的地址单元	
+	mov edx,0
+	mov [ecx],edx
+
+	mov ebx,[bPointer]	;继续初始化fibB
+        add ebx,4               ;往前走 继续初始化为0
+        mov [bPointer],ebx
+        mov ecx,[bPointer]      ;ecx里放的是要初始为0的地址单元 
+        mov edx,0
+        mov [ecx],edx
+
+	mov eax,[loopInitial]
+	dec eax			;初始化循环计数器减一
+	mov [loopInitial],eax
+
+	jmp initialAB		;继续初始化
+
+endIni:	mov eax,fibA		;恢复fibA fibB的起始地址到aPointer bPointer中
+	mov [aPointer],eax
+	mov ebx,fibB
+	mov [bPointer],ebx
+
+countFib:
+	mov eax,0
+	mov ebx,[fibN]
+	cmp ebx,eax
+	je compareFib0	;如果正在fib(0),就去判断fib（0）要不要直接输出
+	
+	mov eax,1
+	mov ebx,[fibN]
+	cmp ebx,eax	;eax里有fibN值 和
+	je compareFib1
+
+	jmp exit
+	
+compareFib0:
+	mov eax,[fibN]
+	mov ebx,[varX]
+	cmp eax,ebx
+	je outputFib0
+	mov eax,[fibN]
+	inc eax
+	mov [fibN],eax
+	jmp countFib
+
+compareFib1:
+	mov eax,[fibN]
+	mov ebx,[varX]
+	cmp eax,ebx	;fib1>=varX
+	jge compareVarY
+	
+	mov eax,[fibN]
+	inc eax
+	mov [fibN],eax
+	
+	jmp countFib
+compareVarY:
+	mov eax,[fibN]
+	mov ebx,[varY]
+	cmp eax,ebx	;fib1<=varY
+	jle outputFib1
+	jmp exit
+
+outputFib0:
+	mov eax,[fibN]
+	inc eax
+	mov [fibN],eax
+
+	mov eax,4
+	mov ebx,1
+	mov ecx,zero
+	mov edx,2
+	int 80h
+	jmp countFib
+
+outputFib1:
+	mov eax,[fibN]
+        inc eax
+        mov [fibN],eax
+	
+	mov eax,4
+	mov ebx,1
+	mov ecx,one
+	mov edx,2
+	int 80h
+	jmp countFib
+
 exit:   mov eax,1	;Specify Exit syscall	;结束
         mov ebx,0	;Return a code of zero
         int 80h		;Make the syscall to terminate the program
